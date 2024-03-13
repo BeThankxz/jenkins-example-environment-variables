36acd10f529c53dba2e1d1cbf92356a30139d2ef
Bood Atsatarakun <b.atsatarakun@gmail.com>
2024-03-13 13:42:30 +0700

    Refactor Jenkinsfile and url_parser.groovy to remove unnecessary comments and debug statements

--- url_parser.groovy
+++ url_parser.groovy
@@ -1,25 +1,23 @@
 def replaceBaseUrl() {
     def jenkinUrl = env.JENKINS_URL
     def jenkinUrlLength = jenkinUrl.length()
-
-    echo "JENKINS_URL : ${jenkinUrl}"
-    echo "JENKINS_URL LENGTH : ${jenkinUrlLength}"
+    // echo "JENKINS_URL : ${jenkinUrl}"
+    // echo "JENKINS_URL LENGTH : ${jenkinUrlLength}"
 
     def buildUrl = env.BUILD_URL
     def buildUrlLength = buildUrl.length()
-
-    echo "BUILD_URL : ${buildUrl}"
-    echo "BUILD_URL LENGTH : ${buildUrl}"
+    // echo "BUILD_URL : ${buildUrl}"
+    // echo "BUILD_URL LENGTH : ${buildUrl}"
 
     def suffixUrl = buildUrl.substring(jenkinUrlLength, buildUrlLength)
-    echo "SUFIX URL : ${suffixUrl}"
+    // echo "SUFIX URL : ${suffixUrl}"
 
     def publicUrl = env.PUBLIC_URL
-    echo "publicUrl : ${publicUrl}"
+    // echo "publicUrl : ${publicUrl}"
 
     def newUrl = publicUrl + suffixUrl
-    echo "NEW URL : ${newUrl}"
-    
+    // echo "NEW URL : ${newUrl}"
+
     return newUrl
 }
 
